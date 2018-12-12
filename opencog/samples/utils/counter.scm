e954f398f4e269a07fc23c9841c19cebcf19d85f
Alexander Scherbatiy <stellarspot@gmail.com>
2018-12-12 16:00:37 +0300

    Add counter sample

---
+++ opencog/samples/utils/counter.scm
@@ -0,0 +1,29 @@
+(use-modules (opencog) (opencog query) (opencog exec))
+
+; See original example:
+; https://github.com/singnet/atomspace/blob/master/examples/atomspace/random-choice.scm
+
+(State (Concept "counter-state") (Number 0))
+
+(Define (DefinedPredicate "counter")
+ (True
+ (Put
+  (State (Concept "counter-state") (Variable "$x"))
+  (Plus (Number 1)
+   (Get (State (Concept "counter-state") (Variable "$y")))))
+ )
+)
+
+; Run Counter
+(cog-evaluate! (DefinedPredicate "counter"))
+
+; Print Counter
+(display (cog-execute! (Get (State (Concept "counter-state") (Variable "$x")))))
+(newline)
+
+; Run Counter
+(cog-evaluate! (DefinedPredicate "counter"))
+
+; Print Counter
+(display (cog-execute! (Get (State (Concept "counter-state") (Variable "$x")))))
+(newline)
