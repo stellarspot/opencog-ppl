794a3e0c624399d168b2b48af3f436cc51818c6e
Alexander Scherbatiy <stellarspot@gmail.com>
2018-12-12 16:58:45 +0300

    Add simple Python sample

---
+++ opencog/samples/python/atomspace-sample.py
@@ -0,0 +1,30 @@
+from opencog.atomspace import AtomSpace, types
+
+a = AtomSpace()
+cat = a.add_node(types.ConceptNode, "Cat")
+animal = a.add_node(types.ConceptNode, "Animal")
+a.add_link(types.InheritanceLink, [cat, animal])
+
+for atom in a:
+    print (atom)
+
+from opencog.utilities import initialize_opencog
+from opencog.type_constructors import *
+from opencog.bindlink import satisfying_set
+
+initialize_opencog(a)
+
+a.clear()
+
+color = ConceptNode("Color")
+
+InheritanceLink(ConceptNode("Red"), color)
+InheritanceLink(ConceptNode("Green"), color)
+InheritanceLink(ConceptNode("Blue"), color)
+
+# Create a pattern to look for color nodes
+varlink = TypedVariableLink(VariableNode("$xcol"), TypeNode("ConceptNode"))
+pattern = InheritanceLink(VariableNode("$xcol"), color)
+colornodes = GetLink(varlink, pattern)
+
+print(satisfying_set(a, colornodes))
