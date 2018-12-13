510e19ab682a81597037403bc3b52972f7998259
Alexander Scherbatiy <stellarspot@gmail.com>
2018-12-13 18:01:41 +0300

    Add opencog_math.py sample

---
+++ opencog/samples/python/opencog_math.py
@@ -0,0 +1,25 @@
+from opencog.atomspace import AtomSpace, types
+
+from opencog.utilities import initialize_opencog
+from opencog.type_constructors import *
+from opencog.bindlink import satisfying_set
+
+a = AtomSpace()
+initialize_opencog(a)
+
+num1 = NumberNode("0.5")
+num2 = NumberNode("0.5")
+
+
+def probability_and(n1, n2):
+    p1 = float(n1.name)
+    p2 = float(n2.name)
+    return NumberNode(str(p1 * p2))
+
+def probability_or(n1, n2):
+    p1 = float(n1.name)
+    p2 = float(n2.name)
+    return NumberNode(str(p1 + p2 - p1 * p2))
+
+print("probability and: ", probability_and(num1, num2))
+print("probability or : ", probability_or(num1, num2))
