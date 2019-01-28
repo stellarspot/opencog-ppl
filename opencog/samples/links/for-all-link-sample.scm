27c454d125ff233491fc1cdf1257038d3c9aa303
Alexander Scherbatiy <stellarspot@gmail.com>
2019-01-28 18:24:26 +0300

    Update ForAllLink sample

--- opencog/samples/links/for-all-link-sample.scm
+++ opencog/samples/links/for-all-link-sample.scm
@@ -1,32 +1,45 @@
 (use-modules (opencog) (opencog query) (opencog exec))
 
 (define cat (Concept "cat"))
-(define dog (Concept "cat"))
-(define animal (Concept "animal"))
-(define pet (Concept "pet"))
+(define dog (Concept "dog"))
+(define mammalia (Concept "mammalia"))
+
+(define ant (Concept "ant"))
+(define insect (Concept "insect"))
+
+(define key (Predicate "legs-number"))
+
+(define (legs-number who legs)
+ (Evaluation key who (Number legs)))
+
+(Inheritance cat mammalia)
+(Inheritance dog mammalia)
+(legs-number cat 4)
+(legs-number dog 4)
+
+(Inheritance ant insect)
+(legs-number ant 6)
 
 
-(Inheritance cat animal)
-(Inheritance cat pet)
+; Find $X that
+; For All (Inheritance $Y $X)
+; (legs-number $Y 4)
 
-(Inheritance dog animal)
-
-
-; ForAllLink throws exception
-; [ERROR] Either incorrect or not implemented yet. Cannot evaluate (ForAllLink
-(define test
- ;  (ForAll
- (Satisfaction
+(define pattern
+ (Bind
   (Variable "$X")
-  (And
-   (Inheritance
-    (Variable "$X")
-    animal)
-   (Inheritance
-    (Variable "$X")
-    pet))))
+  (ForAll
+   (Variable "$Y")
+   (And
+    (Inheritance
+     (Variable "$Y")
+     (Variable "$X"))
+    (Evaluation
+     key
+     (Variable "$Y")
+     (NumberNode 4))))
+  (Variable "$X")))
 
 (display
- (cog-evaluate! test)
+ (cog-execute! pattern)
 )
-(newline)
