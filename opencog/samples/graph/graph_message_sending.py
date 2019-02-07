1217903cc14dcb08f26b5b322909f34dc38b4224
Alexander Scherbatiy <stellarspot@gmail.com>
2019-02-07 16:18:20 +0300

    Add Initial message_sending_rule for message sending sample

--- opencog/samples/graph/graph_message_sending.py
+++ opencog/samples/graph/graph_message_sending.py
@@ -6,6 +6,9 @@ from opencog.bindlink import execute_atom
 atomspace = AtomSpace()
 initialize_opencog(atomspace)
 
+TV_FALSE = TruthValue(0, 1)
+TV_TRUE = TruthValue(1, 1)
+
 EDGE_KEY = PredicateNode('edge')
 MESSAGE_KEY = PredicateNode('message')
 DIRECTED_MESSAGE_EDGE_KEY = PredicateNode('directed-message-edge')
@@ -27,6 +30,10 @@ def get_directed_message_edge(a, b):
     return EvaluationLink(DIRECTED_MESSAGE_EDGE_KEY, ListLink(a, b))
 
 
+def has_message(edge):
+    return TV_TRUE if edge.get_value(MESSAGE_KEY) else TV_FALSE
+
+
 # Graph
 # A - +     + - D
 #     C --- F
@@ -38,7 +45,7 @@ get_edge(get_node("D"), get_node("F"))
 get_edge(get_node("E"), get_node("F"))
 get_edge(get_node("C"), get_node("F"))
 
-message_graph_creation_rule = BindLink(
+directed_message_edge_creation_rule = BindLink(
     VariableList(
         TypedVariableLink(
             VariableNode('$X'),
@@ -46,12 +53,45 @@ message_graph_creation_rule = BindLink(
         TypedVariableLink(
             VariableNode('$Y'),
             TypeNode('ConceptNode'))),
-    get_edge(VariableNode('$X'), VariableNode('$Y')),
+    get_edge(
+        VariableNode('$X'),
+        VariableNode('$Y')),
     ListLink(
-        get_directed_message_edge(VariableNode('$X'), VariableNode('$Y')),
-        get_directed_message_edge(VariableNode('$Y'), VariableNode('$X'))
-    )
-)
+        get_directed_message_edge(
+            VariableNode('$X'),
+            VariableNode('$Y')),
+        get_directed_message_edge(
+            VariableNode('$Y'),
+            VariableNode('$X'))))
 
-res = execute_atom(atomspace, message_graph_creation_rule)
-# print(res)
+message_sending_rule = BindLink(
+    VariableList(
+        TypedVariableLink(
+            VariableNode('$X'),
+            TypeNode('ConceptNode')),
+        TypedVariableLink(
+            VariableNode('$Y'),
+            TypeNode('ConceptNode'))),
+
+    AndLink(
+        # Preconditions
+        NotLink(
+            EvaluationLink(
+                GroundedPredicateNode('py: has_message'),
+                ListLink(
+                    get_directed_message_edge(
+                        VariableNode('$X'),
+                        VariableNode('$Y'))))),
+        # Pattern clauses
+        get_directed_message_edge(
+            VariableNode('$X'),
+            VariableNode('$Y'))
+    ),
+    get_message(
+        VariableNode('$X'),
+        VariableNode('$Y')))
+
+res = execute_atom(atomspace, directed_message_edge_creation_rule)
+res = execute_atom(atomspace, message_sending_rule)
+
+print(res)
